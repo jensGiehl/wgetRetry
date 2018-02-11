9de1a9d1a7f2ea285d53071362d7b2a6345a9ddd
Jens Giehl <mobil.jgiehl@gmail.com>
2018-02-11 14:21:58 +0100

    #1: HTTP Status 400 is also okay

--- wgetRetry.sh
+++ wgetRetry.sh
@@ -8,13 +8,14 @@ url=$1
 currentDate=`date '+%Y-%m-%d_%H%M%S'`
 outputDocument=${2:-"response-$currentDate.txt"}
 retryCount=${3:-10}
+allowedResponseCodes=("200", "400")
 
-responseCode=0
+responseCode=-1
 currentTry=1
-while [ "$responseCode" -ne 200 ]; do
+while [[ ! "${allowedResponseCodes[@]}" =~ "${responseCode}" ]]; do
 	responseCode=`wget --server-response --no-http-keep-alive --output-document=$outputDocument -q "$url" 2>&1 | grep "HTTP/" | cut -d' ' -f4`
 	logger "HTTP response code for $url is $responseCode (Try: $currentTry / $retryCount)"
-	if [ "$responseCode" -ne 200 ]; then
+	if [[ ! "${allowedResponseCodes[@]}" =~ "${responseCode}" ]]; then
 		sleep 4
 		currentTry=$((currentTry+1))
 		if [ "$currentTry" -gt "$retryCount" ]; then
